1cbc4127dd7ddb0e2fbdad28bddd7b6f71d97be3
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-08-18 22:12:58 +0900

    add msg and user model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < WebsocketRails::BaseController
   def new
+    Message.create message: message[:msg], user_id: 1
     broadcast_message :spread_message, message
   end
 end
